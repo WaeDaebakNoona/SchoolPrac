2ce5fae579b29eccb8c24fd54d5f7d17ce5c5a1d
Naritaa <Naritaa@ZAKZRHB-CLLB24.ballito.local>
2023-08-10 11:42:07 +0200

    school prac. prelim past paper

---
+++ src/main/java/PrelimsPastPaper/Assessment.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PrelimsPastPaper;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Naritaa
+ */
+public class Assessment {
+    
+    //2.1
+    private String moduleID;
+    private String assessmentName;
+    private int total;
+    private int mark;
+    private double weighting;
+    private LocalDate dateWritten;
+    private boolean isExam;
+    
+    //2.2
+
+    public Assessment(String inID, String inAn, int inT, int inM, double inW, LocalDate inD, boolean inE) {
+        this.moduleID = inID;
+        this.assessmentName = inAn;
+        this.total = inT;
+        this.mark = inM;
+        this.weighting = inW;
+        this.dateWritten = inD;
+        this.isExam = inE;
+    }
+
+    public String getAssessmentName() {
+        return assessmentName;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public double getWeighting() {
+        return weighting;
+    }
+
+    public LocalDate getDateWritten() {
+        return dateWritten;
+    }
+
+    public boolean isIsExam() {
+        return isExam;
+    }
+
+    @Override
+    public String toString() {
+        return "Assessment{" + '}';
+    }
+    
+    
+}
